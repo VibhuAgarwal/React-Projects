60c2c154ef88196cdc41e3de2f76eda7611d9c4d
Vibhor Agarwal <40199138+VibhuAgarwal@users.noreply.github.com>
2023-12-20 17:53:16 +0530

    Add files via upload

---
+++ Accordian/src/data.js
@@ -0,0 +1,28 @@
+const questions = [
+  {
+    id: 1,
+    title: "Do I have to allow the use of cookies?",
+    info: "Not accepting cookies can block your access to some websites. If you urgently need to visit such sites, you can first analyze the site's legitimacy before doing it. Improved user experience. Cookies can be helpful because they help sites remember you."
+  },
+  {
+    id: 2,
+    title: "How do I change my My Page password?",
+    info: "Open your Google Account and sign in. Under “Security, select “Signing in to Google. Choose “Password. You might be prompted to sign in again. Enter your new password and select Change password.Save your new password in a password manager. It stores all of your saved passwords in a secure digital vault."
+  },
+  {
+    id: 3,
+    title: "What is BankID?",
+    info: "Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial."
+  },
+  {
+    id: 4,
+    title: "Whose birth number can I use?",
+    info: "Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify."
+  },
+  {
+    id: 5,
+    title: "When do I recieve a password ordered by letter?",
+    info: "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan "
+  }
+];
+export default questions;
